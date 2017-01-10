4c5763b6a55b399a7495ddf7a4e8728e54af9c31
nater849 <nathancampbell@amdg.rockhursths.edu>
2017-01-10 11:46:57 -0600

    day 4

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 if (window.location.hostname === '127.0.0.1') { 
     window.location  = 'http://localhost:1898';
 }
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail())
+    
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+    
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+  }
+
+function appStart() {
+    gapi.load('auth2', initSigninV2); 
+} 
+
+var auth2;
